ff2ee9af13795e3ba3d97395d3e401c7057d9994
Ashikuzzaman_Moon <ashikuzzamanmoon31@gmail.com>
2023-06-09 02:43:55 +0600

    banner section done

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -5,9 +5,9 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 const Banner = () => {
-    return (
-        <div>
-            <Swiper
+  return (
+    <div>
+      <Swiper
         slidesPerView={1}
         spaceBetween={30}
         loop={true}
@@ -19,29 +19,53 @@ const Banner = () => {
         className="mySwiper"
       >
         <SwiperSlide>
-            <img src="https://images.unsplash.com/photo-1617459840892-6d97e6359eae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTQwfHxwaG90b2dyYXBoeSUyMGNhbWVyYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
-            <h2 className="absolute top-1/2 bottom-1/2 right-1/2 left-1/2 text-red-700">CAPTURE YOUR DREAM PHOTO</h2>
+          <img src="https://images.unsplash.com/photo-1617459840892-6d97e6359eae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTQwfHxwaG90b2dyYXBoeSUyMGNhbWVyYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+          <div className="absolute h-full left-0 top-0 right-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] hover:bg-gradient-to-l rounded-xl">
+            <div className="text-white space-y-5 text-center md:mt-64 mt-20">
+              <h2 className="text-2xl md:text-6xl font-bold">CAPTURE YOUR DREAM PHOTO</h2>
+              <p className="text-xs md:text-xl w-3/4 text-center mx-auto">Join ChitroGolpo Photography School and learn how to capture
+                powerful images that convey emotions and narratives.Discover your artistic vision and express it through the lens.</p>
+              <button className="btn btn-info btn-xs sm:btn-sm md:btn-md lg:btn-lg text-white">Explore Now</button>
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-            <img src="https://images.unsplash.com/photo-1547145695-0232437d417f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=871&q=80" alt="" />
-            <h2 className="absolute top-1/2 bottom-1/2 right-1/2 left-1/2 text-red-700">CAPTURE YOUR DREAM PHOTO</h2>
+          <img src="https://images.unsplash.com/photo-1547145695-0232437d417f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=871&q=80" alt="" />
+          <div className="absolute h-full left-0 top-0 right-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] hover:bg-gradient-to-l rounded-xl">
+            <div className="text-white space-y-5 text-center md:mt-64 mt-20">
+              <h2 className="text-2xl md:text-6xl font-bold">CAPTURE YOUR DREAM PHOTO</h2>
+              <p className="text-xs md:text-xl w-3/4 text-center mx-auto">Join ChitroGolpo Photography School and learn how to capture
+                powerful images that convey emotions and narratives.Discover your artistic vision and express it through the lens.</p>
+              <button className="btn btn-info btn-xs sm:btn-sm md:btn-md lg:btn-lg text-white">Explore Now</button>
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-            <img src="https://img.freepik.com/premium-photo/camera-with-lens-pastel-colored-surface_175682-5405.jpg?w=740" alt="" />
-            <h2 className="absolute top-1/2 bottom-1/2 right-1/2 left-1/2 text-red-700">CAPTURE YOUR DREAM PHOTO</h2>
+          <img src="https://img.freepik.com/free-photo/professional-photographer-takes-photos-with-camera-tripod-rocky-peak-sunset-dark-tone_335224-432.jpg?w=740&t=st=1686251124~exp=1686251724~hmac=e0b4f1e375c34774825ee84fe5d9c3c31d12c29c5a98664572b461dd4b6e221a" alt="" />
+          <div className="absolute h-full left-0 top-0 right-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] hover:bg-gradient-to-l rounded-xl">
+            <div className="text-white space-y-5 text-center md:mt-64 mt-20">
+              <h2 className="text-2xl md:text-6xl font-bold">CAPTURE YOUR DREAM PHOTO</h2>
+              <p className="text-xs md:text-xl w-3/4 text-center mx-auto">Join ChitroGolpo Photography School and learn how to capture
+                powerful images that convey emotions and narratives.Discover your artistic vision and express it through the lens.</p>
+              <button className="btn btn-info btn-xs sm:btn-sm md:btn-md lg:btn-lg text-white">Explore Now</button>
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-            <img src="https://img.freepik.com/free-photo/professional-photographer-takes-photos-with-camera-tripod-rocky-peak-sunset-dark-tone_335224-432.jpg?w=740&t=st=1686251124~exp=1686251724~hmac=e0b4f1e375c34774825ee84fe5d9c3c31d12c29c5a98664572b461dd4b6e221a" alt="" />
-            <h2 className="absolute top-1/2 bottom-1/2 right-1/2 left-1/2 text-red-700">CAPTURE YOUR DREAM PHOTO</h2>
+          <img src="https://img.freepik.com/free-photo/silhouette-photographer-who-shoots-sunset-mountains_1150-7151.jpg?size=626&ext=jpg&uid=R101321192&ga=GA1.2.1999469744.1683008976&semt=ais" alt="" />
+          <div className="absolute h-full left-0 top-0 right-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] hover:bg-gradient-to-l rounded-xl">
+            <div className="text-white space-y-5 text-center md:mt-64 mt-20">
+              <h2 className="text-2xl md:text-6xl font-bold">CAPTURE YOUR DREAM PHOTO</h2>
+              <p className="text-xs md:text-xl w-3/4 text-center mx-auto">Join ChitroGolpo Photography School and learn how to capture
+                powerful images that convey emotions and narratives.Discover your artistic vision and express it through the lens.</p>
+              <button className="btn btn-info btn-xs sm:btn-sm md:btn-md lg:btn-lg text-white">Explore Now</button>
+            </div>
+          </div>
         </SwiperSlide>
-        <SwiperSlide>
-            <img src="https://img.freepik.com/premium-photo/photographer-taking-self-portrait-woman-using-camera-taking-photo-dark-tone-front-view-selective-focus-lense-straight-into-camera_34048-994.jpg?w=740" alt="" />
-            <h2 className="absolute top-1/2 bottom-1/2 right-1/2 left-1/2 text-red-700">CAPTURE YOUR DREAM PHOTO</h2>
-        </SwiperSlide>
-        
+
       </Swiper>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Banner;
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -16,10 +16,10 @@ const Header = () => {
 
     </>
     return (
-        <div className="navbar bg-base-200">
+        <div className="navbar fixed z-10  md:bg-opacity-30 md:bg-black md:text-white max-w-screen-xl">
             <div className="navbar-start">
                 <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                    <label tabIndex={0} className="btn btn-ghost text-white lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 font-semibold">
@@ -28,12 +28,12 @@ const Header = () => {
                 </div>
                 <Link to="/">
                     <div className="flex justify-center items-center">
-                        <img className="h-20 bg-base-200" src="https://i.ibb.co/HgX4FbC/logo-chitrogolpo-removebg-preview.png" alt="" />
+                        <img className="h-20" src="https://i.ibb.co/HgX4FbC/logo-chitrogolpo-removebg-preview.png" alt="" />
                     </div>
                 </Link>
                 <Link to="/">
                     <div className="">
-                        <p className="text-xl font-bold">Chitro Golpo</p>
+                        <p className="text-xs md:text-xl text-white font-bold">Chitro Golpo</p>
                     </div>
                 </Link>
             </div>
