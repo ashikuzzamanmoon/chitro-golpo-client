baddf69fcdbece1a05524cf5734244f6ad26e097
Ashikuzzaman_Moon <ashikuzzamanmoon31@gmail.com>
2023-06-10 02:45:53 +0600

    modified extra section

--- src/pages/Home/LearnBasic/LearnBasic.jsx
+++ src/pages/Home/LearnBasic/LearnBasic.jsx
@@ -1,25 +1,43 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useState } from 'react';
+import { useSpring, animated } from 'react-spring';
 
 
 const LearnBasic = () => {
-    const [animation, setAnimation] = useState('');
+    const [isHovered, setIsHovered] = useState(false);
 
-    useEffect(() => {
-      const interval = setInterval(() => {
-        setAnimation(prevAnimation => {
-          return prevAnimation === '' ? '|' : prevAnimation === '|' ? '/' : prevAnimation === '/' ? '-' : '\\';
-        });
-      }, 300);
-  
-      return () => clearInterval(interval);
-    }, []);
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
+    const foldInStyle = useSpring({
+        transform: isHovered ? 'perspective(600px) rotateY(-180deg)' : 'perspective(600px) rotateY(0deg)',
+    });
+
+    const foldOutStyle = useSpring({
+        transform: isHovered ? 'perspective(600px) rotateY(0deg)' : 'perspective(600px) rotateY(-180deg)',
+    });
 
     return (
         <>
             <h2 className="text-5xl font-bold mb-6 text-center">Learn Basic</h2>
-            <div>
-                <pre>{animation}</pre>
+            <div
+                className="animation-card"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+            >
+                <animated.div className="card-image" style={foldInStyle}>
+                    <img src="https://img.freepik.com/free-photo/b-w_1122-1880.jpg?size=626&ext=jpg&uid=R101321192&ga=GA1.2.1999469744.1683008976&semt=ais" alt="Photography Image" />
+                </animated.div>
+                <animated.div className="card-content" style={foldOutStyle}>
+                    <div className='top-0'>
+                        <h3 className="card-title">Street Photography</h3>
+                        <p className="card-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, rerum commodi voluptas ad temporibus praesentium reprehenderit quod animi quas illo excepturi, architecto nobis dolorem tenetur explicabo, quo molestiae necessitatibus. Obcaecati?</p>
+                    </div>
+                </animated.div>
             </div>
 
         </>
